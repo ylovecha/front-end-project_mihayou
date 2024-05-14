32dab19ac38e9d14307d3f3e6c77764fbb07e419
ylovecha <tianelove0819@163.com>
2024-05-14 20:20:32 +0800

    aboutme部分大体完成，差动效和右上角

--- src/components/home-about.vue
+++ src/components/home-about.vue
@@ -1,15 +1,36 @@
 <script setup>
 import home_about_decoration from '@/components/img/decoration.b195643.png'
 import aside_slogan__line from '@/components/img/line.a3e37b6.png'
+import { onMounted } from 'vue'
+
 import Swiper from 'swiper'
 import 'swiper/swiper-bundle.css'
 
-import { onMounted } from 'vue'
-
 onMounted(() => {
   new Swiper('.swiper-container', {
-    //Swiper 的配置选项
-    //例如：
+    direction: 'vertical',
+    followFinger: false,
+    speed: 800,
+    mousewheel: true,
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    on: {
+      init(swiper) {
+        const slide = swiper.slides.eq(0)
+        slide.addClass('ani-slide')
+      },
+      transitionStart(swiper) {
+        for (let i = 0; i < swiper.slides.length; i++) {
+          const slide = swiper.slides.eq(i)
+          slide.removeClass('ani-slide')
+        }
+      },
+      transitionEnd(swiper) {
+        const slide = swiper.slides.eq(swiper.activeIndex)
+        slide.addClass('ani-slide')
+      }
+    }
   })
 })
 </script>
@@ -29,51 +50,52 @@ onMounted(() => {
       </div>
       <div class="fp-page__wrap">
         <!-----轮播的选择---->
-        <div class="home-about-cates">
-          <div class="home-about-cates-wrap home-about-cates-wrap--active">
-            <div class="home-about-cate">
-              <div
-                class="home-about-cate-wrap"
-                style="transform: translateY(-0.6rem); transition-duration: 0ms"
-              >
-                <div class="span-wrapper"><span> 关于米哈游 </span></div>
-                <div class="span-wrapper">
-                  <span class="tab-dark"> 关于米哈游 </span>
+        <div class="swiper-container swiper-container-initialized swiper-container-vertical">
+          <div class="home-about-cates">
+            <div class="home-about-cates-wrap home-about-cates-wrap--active">
+              <div class="home-about-cate">
+                <div
+                  class="home-about-cate-wrap"
+                  style="transform: translateY(-0.6rem); transition-duration: 0ms"
+                >
+                  <div class="span-wrapper"><span> 关于米哈游 </span></div>
+                  <div class="span-wrapper">
+                    <span class="tab-dark"> 关于米哈游 </span>
+                  </div>
+                  <div class="span-wrapper"><span> 关于米哈游 </span></div>
                 </div>
-                <div class="span-wrapper"><span> 关于米哈游 </span></div>
               </div>
             </div>
-          </div>
-          <div class="home-about-cates-wrap">
-            <div class="home-about-cate">
-              <div
-                class="home-about-cate-wrap"
-                style="transform: translateY(-0.6rem); transition-duration: 0ms"
-              >
-                <div class="span-wrapper"><span> 发展历程 </span></div>
-                <div class="span-wrapper">
-                  <span class="tab-dark"> 发展历程 </span>
+            <div class="home-about-cates-wrap">
+              <div class="home-about-cate">
+                <div
+                  class="home-about-cate-wrap"
+                  style="transform: translateY(-0.6rem); transition-duration: 0ms"
+                >
+                  <div class="span-wrapper"><span> 发展历程 </span></div>
+                  <div class="span-wrapper">
+                    <span class="tab-dark"> 发展历程 </span>
+                  </div>
+                  <div class="span-wrapper"><span> 发展历程 </span></div>
                 </div>
-                <div class="span-wrapper"><span> 发展历程 </span></div>
               </div>
             </div>
-          </div>
-          <div class="home-about-cates-wrap">
-            <div class="home-about-cate">
-              <div
-                class="home-about-cate-wrap"
-                style="transform: translateY(-0.6rem); transition-duration: 0ms"
-              >
-                <div class="span-wrapper"><span> 荣誉资质 </span></div>
-                <div class="span-wrapper">
-                  <span class="tab-dark"> 荣誉资质 </span>
+            <div class="home-about-cates-wrap">
+              <div class="home-about-cate">
+                <div
+                  class="home-about-cate-wrap"
+                  style="transform: translateY(-0.6rem); transition-duration: 0ms"
+                >
+                  <div class="span-wrapper"><span> 荣誉资质 </span></div>
+                  <div class="span-wrapper">
+                    <span class="tab-dark"> 荣誉资质 </span>
+                  </div>
+                  <div class="span-wrapper"><span> 荣誉资质 </span></div>
                 </div>
-                <div class="span-wrapper"><span> 荣誉资质 </span></div>
               </div>
             </div>
           </div>
         </div>
-
         <div class="home-about-container">
           <div class="about-intro">
             <div class="about-intro__vision">
@@ -137,6 +159,37 @@ onMounted(() => {
   width: 100%;
   overflow: hidden;
 }
+/*swiper设置 */
+/* .swiper-container {
+  width: 500px;
+  height: 300px;
+  margin: 20px auto;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide:nth-child(2) {
+  background: #3183ff;
+  color: #fff;
+}
+
+.swiper-slide p {
+  transform: translateX(-200px);
+  opacity: 0;
+  transition: all 0.4s;
+}
+
+.ani-slide p {
+  transform: translateX(0);
+  opacity: 1;
+} */
 
 /*zuoce*/
 .aside-slogan[data-v-5dd2d15e] {
@@ -194,42 +247,48 @@ onMounted(() => {
   display: -ms-flexbox;
   display: flex;
   -webkit-box-pack: end;
-  -ms-flex-pack: end;
   justify-content: flex-end;
   -webkit-box-align: center;
   -ms-flex-align: center;
   align-items: center;
-  margin-bottom: 0.5rem;
-  margin-right: -0.3rem;
+  margin-bottom: 10rem;
+  margin-right: 2rem;
   top: 10px;
-  height: 10rem;
 }
-.home-about-cates {
-  -webkit-transition: -webkit-transform 800ms;
-  transition: -webkit-transform 800ms;
-  -o-transition: transform 800ms;
-  transition: transform 800ms;
-  transition:
-    transform 800ms,
-    -webkit-transform 800ms;
-  -webkit-transition-delay: 200ms;
-  -o-transition-delay: 200ms;
-  transition-delay: 200ms;
-}
+
 .home-about-cates-wrap {
+  /**应用于子元素，用于包装关于米哈游的内容 */
   position: relative;
-  height: 0.3rem;
-  overflow: hidden;
+
+  /* overflow: hidden; */ /**超出内容部分隐藏。 */
   margin-right: -1px;
-  cursor: pointer;
+  cursor: pointer; /*可以点击 */
 }
+.home-about-cate {
+  height: 100px;
+  width: 40px;
+  overflow: hidden;
+}
+.home-about-cate-wrap {
+  font-size: 0;
+  color: #676b73;
+  white-space: nowrap; /**防止文本换行 */
+  position: relative;
+  height: 100px;
+  width: 40px;
+  -webkit-transform: translateY(-0.6rem); /**设置垂直方向上的位移，可能是为了添加动画效果 */
+  -ms-transform: translateY(-0.6rem);
+  transform: translateY(-0.6rem);
+}
+
 .home-about-cates-wrap::before,
 .home-about-cates-wrap::after {
+  /**用于在 .home-about-cates-wrap 元素的前后添加一个竖线 */
   content: '';
   position: absolute;
   top: 50%;
-  width: 1px;
-  height: 0.18rem;
+  width: 3px;
+  height: 100px;
   background: #707173;
   -webkit-transform: translateY(-50%);
   -ms-transform: translateY(-50%);
@@ -241,6 +300,7 @@ onMounted(() => {
 .home-about-cates-wrap::after {
   right: 0;
 }
+
 .home-about-cates-wrap:hover::before,
 .home-about-cates-wrap:hover::after,
 .home-about-cates-wrap--active::before,
@@ -252,6 +312,7 @@ onMounted(() => {
   background-color: #3778e5;
 }
 .home-about-cates-wrap--active {
+  /**被点击选中 */
   color: #fff;
 }
 .home-about-cates-wrap--active .home-about-cate-wrap {
@@ -263,23 +324,10 @@ onMounted(() => {
   color: #fff;
   background-color: #3778e5;
 }
-.home-about-cate {
-  height: 0.3rem;
-  overflow: hidden;
-}
-.home-about-cate-wrap {
-  font-size: 0;
-  color: #676b73;
-  white-space: nowrap;
-  position: relative;
-  height: 0.9rem;
-  -webkit-transform: translateY(-0.6rem);
-  -ms-transform: translateY(-0.6rem);
-  transform: translateY(-0.6rem);
-}
 
 .home-about-cate .span-wrapper {
-  height: 0.3rem;
+  height: 30px;
+  width: 300px;
   display: -webkit-box;
   display: -ms-flexbox;
   display: flex;
@@ -290,11 +338,11 @@ onMounted(() => {
 }
 .home-about-cate span {
   display: block;
-  font-size: 0.18rem;
-  height: 0.25rem;
-  line-height: 0.25rem;
+  font-size: 30rem;
+  height: 100px;
+  line-height: 30rem;
   position: relative;
-  padding: 0 0.38rem;
+  padding: 0 4rem;
 }
 .home-about-cate .tab-dark {
   color: #fff;
@@ -310,7 +358,6 @@ onMounted(() => {
 .about-fade-leave-to {
   opacity: 0;
 }
-.home-about-container,
 .home-about-cates {
   -webkit-transition: -webkit-transform 800ms;
   transition: -webkit-transform 800ms;
@@ -325,9 +372,11 @@ onMounted(() => {
 }
 /*内容*/
 .home-about-container {
-  position: absolute;
-  width: 1440px;
-  height: 1000px;
+  position: relative;
+  width: 1000px;
+  height: 400px;
+  bottom: 160px;
+  left: 100px;
 }
 .home-about-container > div {
   position: absolute;
@@ -348,5 +397,171 @@ onMounted(() => {
   -webkit-transition-delay: 200ms;
   -o-transition-delay: 200ms;
   transition-delay: 200ms;
+}
+
+.about-intro {
+  width: 98%;
+  height: 100%;
+  padding-left: 19rem;
+}
+.about-intro img {
+  width: 100%;
+}
+.about-intro__vision {
+  max-height: 400px;
+}
+.about-intro__vision-title,
+.about-intro__mihoyo-title {
+  position: relative;
+  margin-bottom: 1rem;
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: #242933;
+}
+.about-intro__vision-title b,
+.about-intro__mihoyo-title b {
+  position: absolute;
+  left: 6rem;
+  bottom: -0.6rem;
+  font-size: 0.5rem;
+  font-weight: normal;
+  color: #ccc;
+  letter-spacing: 0.1em;
+  font-family: HarmonyOS, Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+}
+/**彩字设置 */
+.about-intro__vision-slg {
+  position: relative;
+  display: inline-block;
+
+  width: calc(100% - 15.76rem);
+  padding-left: 3.2rem;
+  width: 730px;
+  margin-bottom: 1.4rem;
+  margin-left: 3.64rem;
+  font-size: 2.3rem;
+  line-height: 2.3rem;
+  letter-spacing: 0.1em;
+  font-weight: lighter;
+  color: #4ea1d9;
+}
+.about-intro__vision-slg.pureColor {
+  background: none;
+  pointer-events: none;
+}
+.about-intro__vision-slg.pureColor em {
+  background: none;
+  -webkit-background-clip: none;
+}
+.about-intro__vision-slg strong {
+  color: #74d5ee;
+  font-weight: inherit;
+}
+.about-intro__vision-slg em {
+  color: #418ae0;
+  font-style: normal;
+  background-image: -o-linear-gradient(225deg, #dc8bc3 0, #418ae0 100%);
+  background-image: linear-gradient(-135deg, #dc8bc3 0, #418ae0 100%);
+  -webkit-text-fill-color: rgba(0, 0, 0, 0);
+  -webkit-background-clip: text;
+}
+.about-intro__vision-slg u {
+  color: #dc8bc3;
+  text-decoration: none;
+}
+.about-intro__vision-slg::before,
+.about-intro__vision-slg::after {
+  content: '';
+  position: absolute;
+  width: 0.38rem;
+  height: 1.25rem;
+  background-repeat: 0;
+  background-size: cover;
+  background-position: center center;
+}
+.about-intro__vision-slg::before {
+  left: 0;
+  top: 0;
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAZCAYAAAAMhW+1AAAAAXNSR0IArs4c6QAAACZJREFUOE9j9Ft48z8DHsBIXQWb4tUZ0W1DsWJUASR4RsNhcIcDADoPYG82O/IpAAAAAElFTkSuQmCC);
+}
+.about-intro__vision-slg::after {
+  right: 0;
+  bottom: 0;
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAZCAYAAAAMhW+1AAAAAXNSR0IArs4c6QAAAChJREFUOE9jZMACrsx98h8mzDiqABwCo+EASQhDIxyQUzC2BMxIsQIAEOdTTmAeOPYAAAAASUVORK5CYII=);
+}
+.about-intro__vision-slg .anim {
+  background-image: -o-linear-gradient(
+    225deg,
+    #418ae0,
+    #56a0d8,
+    #dc8bc3,
+    #56a0d8,
+    #418ae0,
+    #56a0d8,
+    #dc8bc3,
+    #56a0d8,
+    #418ae0
+  );
+  background-image: linear-gradient(
+    -135deg,
+    #418ae0,
+    #56a0d8,
+    #dc8bc3,
+    #56a0d8,
+    #418ae0,
+    #56a0d8,
+    #dc8bc3,
+    #56a0d8,
+    #418ae0
+  );
+  background-image: -ms-linear-gradient(
+    -135deg,
+    #418ae0,
+    #56a0d8,
+    #dc8bc3,
+    #56a0d8,
+    #418ae0,
+    #56a0d8,
+    #dc8bc3,
+    #56a0d8,
+    #418ae0
+  );
+  -webkit-text-fill-color: rgba(0, 0, 0, 0);
+  -webkit-background-clip: text;
+  background-size: 200% 100%;
+  -webkit-animation: masked-animation 3s infinite linear;
+  animation: masked-animation 3s infinite linear;
+}
+@-webkit-keyframes masked-animation {
+  0% {
+    background-position: 0 -100%;
+  }
+  100% {
+    background-position: -100% 0;
+  }
+}
+@keyframes masked-animation {
+  0% {
+    background-position: 0 -100%;
+  }
+  100% {
+    background-position: -100% 0;
+  }
+}
+.about-intro__mihoyo-desc {
+  text-align: left;
+  margin-bottom: 0.7rem;
+  font-size: 0.8rem;
+  color: #3d424d;
+  line-height: 1.34rem;
+  height: 2rem;
+}
+.about-intro__mihoyo-desc p {
+  margin-bottom: 0.1rem;
+}
+.about-intro__mihoyo-desc strong {
+  color: #3778e5;
+  font-weight: bold;
 }
 </style>
